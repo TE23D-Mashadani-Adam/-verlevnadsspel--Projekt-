453a68d429c046bd5c276e9ec1d5362c624393c9
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2025-01-20 09:52:20 +0100

    Enkel algoritm för spelet färdig

--- Overlevnadsspel_Projekt/Program.cs
+++ Overlevnadsspel_Projekt/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System.Data;
+
+Random random = new();
+
+Console.WriteLine("Hej och välkommen, du behöver överleva genom att äta" +
+" och göra eld för att bli varm, var försiktig så att du inte förlorar mycket hp" +
+" och dör!, tryck vidare för att starta spelet");
+Console.ReadLine();
+Console.Clear();
+
+float hp = 100;
+float hunger = 100;
+float heat = 100;
+
+while (hp > 0)
+{
+    //Mängd mat och ved
+    int foodCount = random.Next(0, 5); 
+    int treeCount = random.Next(0, 5);
+    int damage = random.Next(40,70);
+    //Mängd förlorad värme och hunger
+    int hungerDamage = random.Next(10, 40);
+    int heatDamage = random.Next(10, 40);
+
+    hunger -= hungerDamage;
+    heat -= heatDamage;
+
+    if (hunger <= 10 || heat <= 30)
+    {
+        hp -= damage;
+    }
+
+    Console.WriteLine($"Hunger: {hunger} Värme: {heat} HP: {hp}");
+    Console.WriteLine("Tryck vidare för att avsluta dagen");
+    Console.ReadLine();
+
+    if (hp <= 0)
+    {
+        Console.WriteLine("Du är död");
+        break;
+    }
+
+}
+
+Console.ReadLine();
+
+
+
