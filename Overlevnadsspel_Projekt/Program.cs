1b297a2c5f87ed95996dc73850ed2371e8a655ca
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2025-01-24 10:34:59 +0100

    Grund algortim för spelet

--- Overlevnadsspel_Projekt/Program.cs
+++ Overlevnadsspel_Projekt/Program.cs
@@ -12,25 +12,67 @@ float hp = 100;
 float hunger = 100;
 float heat = 100;
 
+int daysCount = 1;
+
 while (hp > 0)
 {
     //Mängd mat och ved
-    int foodCount = random.Next(0, 5); 
-    int treeCount = random.Next(0, 5);
-    int damage = random.Next(40,70);
+    int foodCount = random.Next(0, 2);
+    int treeCount = random.Next(0, 2);
+    int damage = random.Next(40, 70);
     //Mängd förlorad värme och hunger
     int hungerDamage = random.Next(10, 40);
     int heatDamage = random.Next(10, 40);
 
+    string availbleItems = "";
+    string answer = "";
+
+    //Algoritm för att äta och göra eld med ved
+    while (answer.ToLower() != "slut")
+    {
+        showItemsMessage(availbleItems, foodCount, treeCount);
+        answer = Console.ReadLine();
+
+        if (answer.ToLower() == "m" && foodCount > 0)
+        {
+            hungerDamage = 0;
+            if (hunger <= 60) { hunger += 40; }
+            else
+            {
+                hunger += 100 - hunger; // Gör så att hungern går upp till max 100
+            }
+            foodCount--;
+        }
+        else if (answer.ToLower() == "m" && foodCount == 0)
+        { Console.WriteLine("Det finns ingen mat"); }
+
+        else if (answer.ToLower() == "v" && treeCount > 0)
+        {
+            heatDamage = 0;
+            if (heat <= 50) { heat += 50; }
+            else
+            {
+                heat += 100 - heat;
+            }
+            treeCount--;
+        } else if (answer.ToLower() == "v" && treeCount == 0)
+        { Console.WriteLine("Det finns inga träd"); }
+
+        Console.WriteLine($"Hunger: {hunger} Värme: {heat} HP: {hp}");
+
+    }
+
+
     hunger -= hungerDamage;
     heat -= heatDamage;
 
+    //Hp minskar när damage och heat sjunker ner
     if (hunger <= 10 || heat <= 30)
     {
         hp -= damage;
     }
 
-    Console.WriteLine($"Hunger: {hunger} Värme: {heat} HP: {hp}");
+    Console.WriteLine($"Hunger: {hunger} Värme: {heat} HP: {hp} Dag: {daysCount}");
     Console.WriteLine("Tryck vidare för att avsluta dagen");
     Console.ReadLine();
 
@@ -40,6 +82,15 @@ while (hp > 0)
         break;
     }
 
+    daysCount++;
+
+}
+
+void showItemsMessage(string availbleItems, float foodCount, float treeCount)
+{
+    availbleItems = $"Mat: {foodCount} Ved: {treeCount}";
+    Console.WriteLine(availbleItems + "\n" + "För mat, skriv m, för ved, skriv v"
+    + "För att avsluta, skriv 'slut'");
 }
 
 Console.ReadLine();
